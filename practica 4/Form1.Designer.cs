31d14e1eb9188a88e5eac4ab2ba0ce5c56618334
Alumnos <Alumnos@DESKTOP-OHBOQ9P>
2024-10-23 11:26:48 -0600

    Agregar archivos de proyecto.

---
+++ practica 4/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace practica_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("TechnicBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(134, 61);
+            label1.Name = "label1";
+            label1.Size = new Size(83, 18);
+            label1.TabIndex = 0;
+            label1.Text = "Numero A";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("TechnicBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(135, 114);
+            label2.Name = "label2";
+            label2.Size = new Size(82, 18);
+            label2.TabIndex = 1;
+            label2.Text = "Numero B";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("TechnicBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(135, 165);
+            label3.Name = "label3";
+            label3.Size = new Size(83, 18);
+            label3.TabIndex = 2;
+            label3.Text = "Numero C";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("TechnicBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(134, 219);
+            label4.Name = "label4";
+            label4.Size = new Size(83, 18);
+            label4.TabIndex = 3;
+            label4.Text = "Numero D";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("TechnicBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(386, 61);
+            label5.Name = "label5";
+            label5.Size = new Size(83, 18);
+            label5.TabIndex = 4;
+            label5.Text = "Numero A";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("TechnicBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(387, 114);
+            label6.Name = "label6";
+            label6.Size = new Size(82, 18);
+            label6.TabIndex = 5;
+            label6.Text = "Numero B";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("TechnicBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(387, 165);
+            label7.Name = "label7";
+            label7.Size = new Size(83, 18);
+            label7.TabIndex = 6;
+            label7.Text = "Numero C";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("TechnicBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(387, 219);
+            label8.Name = "label8";
+            label8.Size = new Size(83, 18);
+            label8.TabIndex = 7;
+            label8.Text = "Numero D";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("SWRomnt", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.Location = new Point(81, 9);
+            label9.Name = "label9";
+            label9.Size = new Size(511, 27);
+            label9.TabIndex = 8;
+            label9.Text = "Escribir numeros en orden inverso";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(232, 56);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(232, 109);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(232, 160);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(232, 214);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(492, 61);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 13;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(492, 109);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 14;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(492, 165);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(100, 23);
+            textBox7.TabIndex = 15;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(492, 219);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(100, 23);
+            textBox8.TabIndex = 16;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Stencil", 9F, FontStyle.Italic, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.HotTrack;
+            button1.Location = new Point(306, 278);
+            button1.Name = "button1";
+            button1.Size = new Size(104, 57);
+            button1.TabIndex = 17;
+            button1.Text = "Invertir Orden";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private Button button1;
+    }
+}
